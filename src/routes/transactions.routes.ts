ec1676189a32aaa1bcb0be04e4c81f0d3b957d2b
Jaelson R Cunha <jaelsonrc@hotmail.com>
2020-11-08 20:27:03 +0100

    Desafio 06: Banco de dados e upload de arquivos no Node.js

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -12,57 +12,39 @@ const transactionsRouter = Router();
 const upload = multer({ dest: 'tmp/' });
 
 transactionsRouter.get('/', async (request, response) => {
-  try {
-    const repository = getCustomRepository(TransactionsRepository);
-    return response.json(await repository.all());
-  } catch (err) {
-    return response.status(400).json({ message: err.message, status: 'error' });
-  }
+  const repository = getCustomRepository(TransactionsRepository);
+  return response.json(await repository.all());
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  try {
-    const { title, value, type, category } = request.body;
+  const { title, value, type, category } = request.body;
 
-    const service = new CreateTransactionService();
+  const service = new CreateTransactionService();
 
-    const transaction = await service.execute({ title, value, type, category });
+  const transaction = await service.execute({ title, value, type, category });
 
-    return response.json(transaction);
-  } catch (err) {
-    return response.status(400).json({ message: err.message, status: 'error' });
-  }
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    const service = new DeleteTransactionService();
+  const service = new DeleteTransactionService();
 
-    await service.execute(id);
+  await service.execute(id);
 
-    return response.status(204).json();
-  } catch (err) {
-    return response.status(400).json({ message: err.message, status: 'error' });
-  }
+  return response.status(204).json();
 });
 
 transactionsRouter.post(
   '/import',
   upload.single('file'),
   async (request, response) => {
-    try {
-      const service = new ImportTransactionsService();
-     // const filePath = `${request.file.path}.csv`;
-      const transactions = await service.execute(request.file.path);
+    const service = new ImportTransactionsService();
 
-      return response.json(transactions);
-    } catch (err) {
-      return response
-        .status(400)
-        .json({ message: err.message, status: 'error' });
-    }
+    const transactions = await service.execute(request.file.path);
+
+    return response.json(transactions);
   },
 );
 
